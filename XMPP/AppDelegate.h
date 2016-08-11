ff03875ed4e7e262bf87911bf86d523fce890f6d
guoduo <guoduo@mattservice.com>
2016-08-11 15:13:36 +0800

    Initial Commit

---
+++ XMPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPP
+//
+//  Created by guoduo on 16/8/11.
+//  Copyright © 2016年 guoduo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMPP
+//
+//  Created by guoduo on 16/8/11.
+//  Copyright © 2016年 guoduo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
